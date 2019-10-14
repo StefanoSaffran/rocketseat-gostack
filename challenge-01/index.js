33a4e186a3e87c84fa7cbae6a1f94b4d1b7c8d32
Stefano Akira Saffran <stefanoas@gmail.com>
2019-10-14 23:18:55 +0200

    adding project

---
+++ challenge-01/index.js
@@ -0,0 +1,74 @@
+const express = require('express');
+
+const server = express();
+
+server.use(express.json());
+
+let numberOfRequests = 0;
+const projects = [];
+
+server.use((req, res, next) => {
+  numberOfRequests++;
+
+  console.log(`${numberOfRequests} request on the server until now.`);
+
+  next();
+  
+})
+
+const checkProjectExists = (req, res, next) => {
+  const { id } = req.params;
+  const project = projects.find(p => p.id === id)
+  //const project = projects.filter(project => project.id === req.params.id);
+
+  if (project === undefined) return res.status(400).json({ error: 'Project not found' });
+
+  req.project = project;
+  return next();
+}
+
+server.get('/projects', (req, res) => {
+  return res.json(projects);
+})
+
+server.post('/projects', (req, res) => {
+  const { id, title } = req.body;
+
+  const newProject = {
+    id,
+    title,
+    tasks: []
+  }
+  projects.push(newProject);
+
+  return res.json(projects);
+})
+
+server.post('/projects/:id/tasks', checkProjectExists, (req, res) => {
+  const { title } = req.body;
+
+  const index = projects.indexOf(req.project);
+
+  projects[index].tasks.push(title);
+
+  return res.json(projects);
+})
+
+server.put('/projects/:id', checkProjectExists, (req, res) => {
+  const { title } = req.body;
+
+  const index = projects.indexOf(req.project);
+  projects[index].title = title;
+
+  return res.json(projects);
+})
+
+server.delete('/projects/:id', checkProjectExists, (req, res) => {
+
+  const index = projects.indexOf(req.project);
+  projects.splice(index, 1);
+
+  return res.send();
+})
+
+server.listen(3003);
