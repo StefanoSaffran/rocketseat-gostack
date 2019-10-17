7f85dfbc37335010d2d3d1ab842f3a03e0eb07b2
Stefano Akira Saffran <stefanoas@gmail.com>
2019-10-17 17:27:15 +0200

    update CRUD/ new middleware

--- challenge-01/index.js
+++ challenge-01/index.js
@@ -12,9 +12,19 @@ server.use((req, res, next) => {
 
   console.log(`${numberOfRequests} request on the server until now.`);
 
-  next();
+  return next();
   
 })
+
+const checkIdExists = (req, res, next) => {
+  const { id } = req.body;
+  const project = projects.find(p => p.id === id)
+
+  if (project) return res.status(400).json({ error: 'ID must be unique' });
+
+  return next();
+
+}
 
 const checkProjectExists = (req, res, next) => {
   const { id } = req.params;
@@ -30,7 +40,7 @@ server.get('/projects', (req, res) => {
   return res.json(projects);
 })
 
-server.post('/projects', (req, res) => {
+server.post('/projects', checkIdExists, (req, res) => {
   const { id, title } = req.body;
 
   const newProject = {
@@ -46,9 +56,7 @@ server.post('/projects', (req, res) => {
 server.post('/projects/:id/tasks', checkProjectExists, (req, res) => {
   const { title } = req.body;
 
-  const index = projects.indexOf(req.project);
-
-  projects[index].tasks.push(title);
+  req.project.tasks.push(title);
 
   return res.json(projects);
 })
@@ -56,8 +64,7 @@ server.post('/projects/:id/tasks', checkProjectExists, (req, res) => {
 server.put('/projects/:id', checkProjectExists, (req, res) => {
   const { title } = req.body;
 
-  const index = projects.indexOf(req.project);
-  projects[index].title = title;
+  req.project.title = title;
 
   return res.json(projects);
 })
