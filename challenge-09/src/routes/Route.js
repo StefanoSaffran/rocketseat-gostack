074959eaf82d0576ab91e47c99e10fbb9ea4ef1d
Stefano Akira Saffran <stefanoas@gmail.com>
2019-11-14 11:21:07 +0100

    chore: update routes

--- challenge-09/src/routes/Route.js
+++ challenge-09/src/routes/Route.js
@@ -5,19 +5,17 @@ import { Route, Redirect } from 'react-router-dom';
 import AuthLayout from '~/pages/_layouts/auth';
 import DefaultLayout from '~/pages/_layouts/default';
 
-// import { store } from '~/store';
+import { store } from '~/store';
 
 const RouteWrapper = ({ component: Component, isPrivate, ...rest }) => {
-  // const { signed } = store.getState().auth;
-
-  const signed = false;
+  const { signed } = store.getState().auth;
 
   if (!signed && isPrivate) {
     return <Redirect to="/" />;
   }
 
   if (signed && !isPrivate) {
-    return <Redirect to="/dashboard" />;
+    return <Redirect to="/students" />;
   }
 
   const Layout = signed ? DefaultLayout : AuthLayout;
