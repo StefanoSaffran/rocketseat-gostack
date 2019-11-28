a247bbf1bb476e40af42a6d58b7b43a6c32d49bf
Stefano Saffran <stefanosaffran@hotmail.com>
2019-11-28 12:40:47 +0100

    chore: update index.js to use redux and redux-persist

--- challenge-10/src/index.js
+++ challenge-10/src/index.js
@@ -1,17 +1,18 @@
 import React from 'react';
-import { StatusBar } from 'react-native';
+import { PersistGate } from 'redux-persist/integration/react';
+import { Provider } from 'react-redux';
 
 import './config/ReactotronConfig';
 
-import Routes from './routes';
+import { store, persistor } from './store';
+import App from './App';
 
-const App = () => {
+export default function Index() {
   return (
-    <>
-      <StatusBar barStyle="dark-content" backgroundColor="#fff" />
-      <Routes />
-    </>
+    <Provider store={store}>
+      <PersistGate persistor={persistor}>
+        <App />
+      </PersistGate>
+    </Provider>
   );
-};
-
-export default App;
+}
