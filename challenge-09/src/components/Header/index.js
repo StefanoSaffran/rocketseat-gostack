e32a6297f9214cd5714ab2ab310cf4b43a3cc1b4
Stefano Saffran <stefanosaffran@hotmail.com>
2019-12-09 01:03:04 +0100

    feat: add new component Menu on Header

--- challenge-09/src/components/Header/index.js
+++ challenge-09/src/components/Header/index.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState, useLayoutEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import LinkWrapper from '~/helpers/LinkWrapper';
+import Menu from './Menu';
 
 import { Container, Content, Profile } from './styles';
 
@@ -8,8 +9,18 @@ import logo from '~/assets/logo-header.png';
 import { signOut } from '~/store/modules/auth/actions';
 
 export default function Header() {
+  const [width, setWidth] = useState([0]);
   const profile = useSelector(state => state.user.profile);
   const dispatch = useDispatch();
+
+  useLayoutEffect(() => {
+    function updateWidth() {
+      setWidth([window.innerWidth]);
+    }
+    window.addEventListener('resize', updateWidth);
+    updateWidth();
+    return () => window.removeEventListener('resize', updateWidth);
+  }, []);
 
   const handleSignout = () => {
     dispatch(signOut());
@@ -18,13 +29,17 @@ export default function Header() {
   return (
     <Container>
       <Content>
-        <nav>
-          <img src={logo} alt="" />
-          <LinkWrapper to="/students">ALUNOS</LinkWrapper>
-          <LinkWrapper to="/plans">PLANOS</LinkWrapper>
-          <LinkWrapper to="/memberships">MATRÍCULAS</LinkWrapper>
-          <LinkWrapper to="/helporders">PEDIDOS DE AUXÍLIO</LinkWrapper>
-        </nav>
+        {width < 768 ? (
+          <Menu />
+        ) : (
+          <nav>
+            <img src={logo} alt="" />
+            <LinkWrapper to="/students">ALUNOS</LinkWrapper>
+            <LinkWrapper to="/plans">PLANOS</LinkWrapper>
+            <LinkWrapper to="/memberships">MATRÍCULAS</LinkWrapper>
+            <LinkWrapper to="/helporders">PEDIDOS DE AUXÍLIO</LinkWrapper>
+          </nav>
+        )}
 
         <aside>
           <Profile>
